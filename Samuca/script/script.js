af68f260fc17ab2f11e4a513db499ea0afe88841
Samuca Silva <silva.samueldev@gmail.com>
2024-08-03 19:10:44 -0300

    UPDATE

---
+++ Samuca/script/script.js
@@ -0,0 +1,38 @@
+
+function Github(){
+    var github = document.getElementById('github')
+    github.src = "img/github.png"
+    github.style.transition = '0.5s ease'
+}
+
+function outgit(){
+    github.src = 'img/git.png'
+    github.style.transition = '0.5s ease'
+}
+
+function Discord(){
+    var discord = document.getElementById('discord')
+    discord.src = 'img/disc.png'
+}
+
+function outdisc(){
+    discord.src = 'img/discord.png'
+}
+
+function Insta(){
+    var insta = document.getElementById('insta')
+    insta.src = 'img/instagram.png'
+}
+
+function outinsta(){
+    insta.src = 'img/insta.png'
+}
+
+function Linkedin(){
+    let linkedin = document.getElementById('linkedin')
+    linkedin.src = 'img/linke.png'
+}
+
+function outlinkedin(){
+    linkedin.src = 'img/linkedin.png'
+}
